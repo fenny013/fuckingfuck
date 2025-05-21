1d3da87b83b188339ab406c0e071cd595deab90a
fenny013 <selezzz@vk.com>
2025-05-21 17:35:12 +0300

    v2

--- script.js
+++ script.js
@@ -35,3 +35,28 @@ function spawnDorito() {
 function startRain() {
     setInterval(spawnDorito, 300);
 }
+const leetMap = {
+    'a': '@', 'b': '8', 'c': 'c', 'd': 'd', 'e': '3',
+    'f': 'f', 'g': '9', 'h': 'h', 'i': '1', 'j': 'j',
+    'k': 'k', 'l': 'l', 'm': 'm', 'n': 'n', 'o': '0',
+    'p': 'p', 'q': 'q', 'r': 'r', 's': '5', 't': '7',
+    'u': 'u', 'v': 'v', 'w': 'w', 'x': 'x', 'y': 'y', 'z': '2',
+
+    'а': '@', 'б': '6', 'в': 'B', 'г': 'r', 'д': 'd',
+    'е': '3', 'ё': '3', 'ж': 'x', 'з': '3', 'и': 'n',
+    'й': 'n\'', 'к': 'k', 'л': 'n', 'м': 'm', 'н': 'H',
+    'о': '0', 'п': 'n', 'р': 'p', 'с': 'c', 'т': '7',
+    'у': 'y', 'ф': 'f', 'х': 'x', 'ц': 'u', 'ч': '4',
+    'ш': 'w', 'щ': 'w\'', 'ъ': 'b', 'ы': 'bl', 'ь': 'b',
+    'э': 'e', 'ю': '10', 'я': '9'
+};
+
+
+function convertToLeet(inputElem) {
+    const raw = inputElem.value;
+    let result = '';
+    for (let ch of raw.toLowerCase()) {
+        result += leetMap[ch] || ch;
+    }
+    inputElem.value = result;
+}
